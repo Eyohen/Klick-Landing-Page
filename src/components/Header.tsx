ced6001fc403301b28da5123e446904442cff492
Joshua <cutejosh2@gmail.com>
2023-06-20 21:56:21 +0100

    added address

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,6 +22,7 @@ const Header = () => {
                 >
                     info@klick.africa
                 </a>
+                <p className="text-sm text-white">150 Ikorodu Road, Onipanu Lagos</p>
             </p>
         </div>
     );
